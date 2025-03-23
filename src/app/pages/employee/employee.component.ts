8fb3eed01b3bc95805b1ca0001a0cc40813f1b00
xiah17 <Jalen.1720@gmail.com>
2025-03-24 00:25:14 +0800

    first commit

---
+++ src/app/pages/employee/employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  imports: [],
+  templateUrl: './employee.component.html',
+  styleUrl: './employee.component.css'
+})
+export class EmployeeComponent {
+
+}
---
+++ src/app/pages/new-leave/new-leave.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-leave',
+  imports: [],
+  templateUrl: './new-leave.component.html',
+  styleUrl: './new-leave.component.css'
+})
+export class NewLeaveComponent {
+
+}
