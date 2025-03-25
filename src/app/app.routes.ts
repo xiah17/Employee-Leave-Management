88c97ff853eff8da6c80c76554ecd1ff1d91fdcf
xiah17 <Jalen.1720@gmail.com>
2025-03-25 19:21:45 +0800

    added new code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { NewLeaveComponent } from './pages/new-leave/new-leave.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch: "full"
+    },
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path:'',
+        component: LayoutComponent,
+        children:[
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            },
+            {
+                path:'employee',
+                component:EmployeeComponent
+            },
+            {
+                path:'leave-request',
+                component:NewLeaveComponent
+            }
+        ]
+    }
+];
